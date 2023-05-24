ac29459ceda0e36b453d83f906add0a6cc6ec5b8
Daniel Thom <daniel.thom@nrel.gov>
2023-05-24 11:34:42 -0600

    Use PowerSystem case in example simulation

--- src/system.jl
+++ src/system.jl
@@ -1,6 +1,6 @@
 _make_system(val::ApiServer.SystemPath; kwargs...) = PSY.System(val.path; kwargs...)
 _make_system(case::ApiServer.SystemCase; kwargs...) =
-    _make_system_from_case(case; kwargs...)
+    make_system_from_case(case.category, case.name; kwargs...)
 
 function make_system_from_case(category::String, name::String; kwargs...)
     psb_category = getproperty(PSB, Symbol(category))
