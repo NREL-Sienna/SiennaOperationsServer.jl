acc402f403d63ce26897c8ab4997c51bebb7c424
Daniel Thom <daniel.thom@nrel.gov>
2023-06-16 07:34:19 -0600

    Run formatter script on custom directories
    This allows the formatter script to run on the auto-generated code
    without affecting the other code.

--- scripts/openapi/build_apis.sh
+++ scripts/openapi/build_apis.sh
@@ -6,19 +6,20 @@ docker run \
     validate \
     -i ${spec_file}
 
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
     echo "Failed to validate ${spec_file}"
     exit 1
 fi
 
 bash scripts/openapi/build_server_api.sh
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
     echo "Failed to build the server API"
     exit 1
 fi
 
+julia scripts/formatter/formatter_code.jl src/api_server/src
 bash scripts/openapi/build_python_client.sh
-if [ $? -ne 0 ]; then
+if [[ $? -ne 0 ]]; then
     echo "Failed to build the Python client"
     exit 1
 fi
